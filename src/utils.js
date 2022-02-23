03aa2ffb18596289d088927c2f676929cc28313f
Julien Martins Da Costa <jmartinsdacosta@wideagency.com>
2022-02-23 12:56:56 +0100

    Add deepmerge for logger config and fix logger import

--- src/utils.js
+++ src/utils.js
@@ -76,3 +76,34 @@ export function parseRefs(el, uid) {
  export function camelize(str) {
   return str.replace(/[\s-]+(\w)/g, (m, c) => c.toUpperCase())
 }
+
+
+/**
+ * Simple is object check.
+ * @param item
+ * @returns {boolean}
+ */
+export function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item) && item !== null)
+}
+
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param source
+ */
+export function mergeDeep(target, source) {
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach(key => {
+      if (isObject(source[key])) {
+        if (!target[key] || !isObject(target[key])) {
+          target[key] = source[key]
+        }
+        mergeDeep(target[key], source[key])
+      } else {
+        Object.assign(target, { [key]: source[key] })
+      }
+    })
+  }
+  return target
+}
